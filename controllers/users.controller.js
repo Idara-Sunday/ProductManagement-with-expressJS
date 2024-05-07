c538c3d3ed2ad89e883bb62111a10b9cb54c03ed
Idara-Sunday <ekpeneidara90@gmail.com>
2024-05-07 15:44:05 +0100

    modified the signUp method in the users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,34 +1,40 @@
 const User = require("../models/user.model.js");
 
 const signUp = async (req, res) => {
-    try{
-    const regUser = await User.create(req.body);
-    res.status(201).json(regUser)
-    } catch(error){
-        res.status(500).json({message:error.message})
+  try {
+    const {email} = req.body;
+    const checkEmail = await User.findOne({ email: email });
+    if (checkEmail) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exist" });
     }
+    const regUser = await User.create(req.body);
+    res.status(201).json(regUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-const loginUser = async (req,res) => {
-    try{
-    const {email,password} = req.body;
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
     // const userEmail = user.email;
     // const userPassword = user.password;
-    // THERE'S A BUG HERE SO TRY AND FIX IT 
-    const checkUser = await User.findOne({email:email});
-    if(!checkUser){
-    return res.status(400).json({message:"Invalid email"})
+    // THERE'S A BUG HERE SO TRY AND FIX IT
+    const checkUser = await User.findOne({ email: email });
+    if (!checkUser) {
+      return res.status(400).json({ message: "Invalid email" });
     }
 
-    if(checkUser.password !== password){
-    return res.status(400).json({message:"Invalid password"})
-    } 
-
-    res.status(200).json(checkUser)
- 
-    } catch(error){
-        res.status(500).json({message:error.message})
+    if (checkUser.password !== password) {
+      return res.status(400).json({ message: "Invalid password" });
     }
+
+    res.status(200).json(checkUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = {signUp,loginUser}
+module.exports = { signUp, loginUser };

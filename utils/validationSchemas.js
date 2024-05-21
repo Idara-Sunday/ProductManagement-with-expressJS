4e5f2d983deba6a3c9830cb63320cffc254eeaac
Idara-Sunday <ekpeneidara90@gmail.com>
2024-05-21 14:11:54 +0100

    created createUserValidationSchema with validation in it

---
+++ utils/validationSchemas.js
@@ -0,0 +1,21 @@
+export const createUserValidationSchema ={
+    email:{
+    notEmpty:{
+        errorMessage:"email field must not be empty"
+    },
+    isString:{
+        errorMessage:"Email must be a string"
+    }
+    },
+    
+    password:{
+        isString:true,
+        isLength:{
+            options:{
+                min:3,
+                max:6
+            },
+            errorMessage:"Password must be at least 3 to 6 characters"
+        }
+    }
+}
